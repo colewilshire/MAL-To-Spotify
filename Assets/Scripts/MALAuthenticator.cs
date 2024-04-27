bfd06ec634d3d962c023a88aed07087cdc39a28f
COLE-DESKTOP\colew <cole.wilshire@outlook.com>
2024-04-26 22:53:33 -0500

    MALToSpotifyDeepLinkActivity now actually works.

--- Assets/Scripts/MALAuthenticator.cs
+++ Assets/Scripts/MALAuthenticator.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
@@ -7,49 +8,40 @@ public class MALAuthenticator : MonoBehaviour
 {
     [SerializeField] private string clientId;
     [SerializeField] private Button button;
-    [SerializeField] private TMP_Text codeText; // Make sure to assign this in the inspector.
+    [SerializeField] private TMP_Text codeText;
+    [SerializeField] private TMP_Text animeText;
     [SerializeField] private string insertAuthCodeHere;
+    [SerializeField] private TMP_InputField inputField;
 
     public Authenticator Authenticator;
 
     public void HandleCode(string code)
     {
-        // Use the received code as needed
-        if (code != null)
-        {
-            codeText.text = code;
-        }
-        else
-        {
-            codeText.text = "null";
-        }
+        inputField.text = code;
+
+        GetMALClient(code);
     }
 
     private void Start()
     {
         Authenticator = new(clientId);
-
-        button.onClick.AddListener(async () => await ButtonClicked(insertAuthCodeHere));
-
         string authUrl = Authenticator.GetAuthorizationURL();
+
         Application.OpenURL(authUrl);
     }
 
-    private async Task ButtonClicked(string authorizationCode)
+    private async void GetMALClient(string authorizationCode)
     {
-        //string authUrl = mALAuthenticator.GetAuthorizationURL();
-        //Application.OpenURL(authUrl); // Open the authorization URL in the user's browser.
-
-        // Wait for the user to enter the authorization code manually or via some other mechanism
-        // For demonstration purposes, let's assume 'insertAuthCodeHere' is obtained somehow.
-        //string insertAuthCodeHere = await Task.FromResult("YourMethodToGetTheCode()");
-
         await Authenticator.ExchangeAuthorizationCodeForToken(authorizationCode);
 
         if (Authenticator.TokenResponse != null)
         {
             codeText.text = $"Access Token: {Authenticator.TokenResponse.AccessToken}";
             GetComponent<MALController>().MALClient = new MALClient(Authenticator.TokenResponse.AccessToken);
+
+            // Testing
+            var animeList = await GetComponent<MALController>().MALClient.GetAnimeListAsync("@me");
+            animeText.text = $"First anime in list: {animeList.Data[0].Node.Title}";
         }
     }
 }
